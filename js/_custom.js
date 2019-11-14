21aeac6ddc7e59c1e792ae4c8119dfdf2c03642e
Gerasik <gerasik1992@gmail.com>
2019-11-14 16:39:45 +0300

    creacte catalog

--- js/_custom.js
+++ js/_custom.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+	const buttons = document.querySelectorAll('.item__button');
+	for (button of buttons){
+		button.addEventListener('click',(e)=>{
+			const rex = /(<([^>]+)>)/ig;
+			const value = e.target.parentNode.children[1].innerHTML.replace(rex , "");
+		})
+	}
 	// Custom JS
 
 });
